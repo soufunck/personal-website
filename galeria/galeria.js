1016ff0845689add72ac3f971e63faa20110aa63
soufunck <62726410+soufunck@users.noreply.github.com>
2021-08-23 23:19:50 -0300

    Arquivo de animações e funções da galeria

---
+++ galeria/galeria.js
@@ -0,0 +1,50 @@
+// efeito de scroll na pagina
+window.sr = ScrollReveal({ reset: true, duration: 2000, distance: '100px'});
+
+sr.reveal(`.c`, {origin: 'right',interval:300});
+
+// filtro de categoria 
+filtro("todas")
+function filtro(c) {
+  var x, i;
+  x = document.getElementsByClassName("c");
+  if (c == "todas") c = "";
+  for (i = 0; i < x.length; i++) {
+    removerclasse(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) addclasse(x[i], "show");
+  }
+}
+
+// adicionar a classe do filtro
+function addclasse(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+  }
+}
+
+// remover a classe do filtro
+function removerclasse(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);     
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+// botoes de categoria
+let containerbotao = document.getElementById("container-botao");
+let botoes = containerbotao.getElementsByClassName("botao");
+for (let i = 0; i < botoes.length; i++) {
+  botoes[i].addEventListener("click", function(){
+    let current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
